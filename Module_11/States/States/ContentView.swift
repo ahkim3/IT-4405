7b2dc50f95f5c392c2fb4e2d2f91f162941873c0
Andrew Kim <andrewhkim3@gmail.com>
2024-04-05 19:22:09 -0500

    Add states for Module 11

---
+++ Module_11/States/States/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  States
+//
+//  Created by Andrew Kim on 4/5/24.
+//
+
+import SwiftUI
+
+let states  = [ "(AK) Alaska", "(AL) Alabama", "(AR) Arkansas", "(AS) American Samoa", "(AZ) Arizona", "(CA) California", "(CO) Colorado", "(CT) Connecticut", "(DC) District of Columbia", "(DE) Delaware", "(FL) Florida", "(GA) Georgia", "(GU) Guam", "(HI) Hawaii", "(IA) Iowa", "(ID) Idaho", "(IL) Illinois", "(IN) Indiana", "(KS) Kansas", "(KY) Kentucky", "(LA) Louisiana", "(MA) Massachusetts", "(MD) Maryland", "(ME) Maine", "(MI) Michigan", "(MN) Minnesota", "(MO) Missouri", "(MS) Mississippi", "(MT) Montana", "(NC) North Carolina", "(ND) North Dakota", "(NE) Nebraska", "(NH) New Hampshire", "(NJ) New Jersey", "(NM) New Mexico", "(NV) Nevada", "(NY) New York", "(OH) Ohio", "(OK) Oklahoma", "(OR) Oregon", "(PA) Pennsylvania", "(PR) Puerto Rico", "(RI) Rhode Island", "(SC) South Carolina", "(SD) South Dakota", "(TN) Tennessee", "(TX) Texas", "(UT) Utah", "(VA) Virginia", "(VI) Virgin Islands", "(VT) Vermont", "(WA) Washington", "(WI) Wisconsin", "(WV) West Virginia", "(WY) Wyoming" ]
+
+struct ContentView: View {
+    @State var selectedState = 0
+
+    var body: some View {
+        VStack {
+            Picker("States", selection: $selectedState) {
+                ForEach(0 ..< states.count) {
+                    Text(states[$0])
+                }
+            }
+
+            Text("Selected state: \(states[selectedState])")
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ Module_11/States/States/StatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatesApp.swift
+//  States
+//
+//  Created by Andrew Kim on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct StatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

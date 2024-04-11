b404fa3e257a90d762db857b5f2d329264e644b8
Andrew Kim <andrewhkim3@gmail.com>
2024-04-10 20:30:22 -0500

    Add Building Lists and Navigation to Module 12

---
+++ Module_12/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Andrew Kim on 4/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}

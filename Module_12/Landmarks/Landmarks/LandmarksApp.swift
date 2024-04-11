970e7e4894103649f424a7d592198db00bbe75ab
Andrew Kim <andrewhkim3@gmail.com>
2024-04-10 19:57:51 -0500

    Add Creating and Combining Views for Module 12

---
+++ Module_12/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Andrew Kim on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
